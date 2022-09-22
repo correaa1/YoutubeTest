29a1e806127c10768d816336fdd61d623bbc5448
correaa1 <bruno_correa_ds@hotmail.com>
2022-09-22 12:55:50 -0300

    finalmente aprendi a consumi essa porra

--- App.js
+++ App.js
@@ -1,72 +1,32 @@
-import React, { useState } from 'react';
-import { Alert, View, ScrollView, Text, Image, Button, StyleSheet, } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios'
 
-const pokemonsIniciais = [
-  { id: 1, nome: "Bulbasauro"},
-  { id: 4, nome: "Charmander"},
-  { id: 7, nome: "Squirtle"},
-];
 
 export default function App() {
-  const [ pokemonEscolhido, setPokemonEscolhido ] = useState(null);
+  const [search, setSearch] = useState('');
 
-  const getPokemonData = (idPokemon) => {
-    const endpoint = `https://www.googleapis.com/youtube/v3/search/${idPokemon}/`;
-
-    fetch(endpoint)
-      .then(resposta => resposta.json())
-        .then( json => {
-          const data = {
-            part: json.part,
-            q: json.q,
-            key: json.key,
-          };
-
-          setPokemonEscolhido(data);
-        })
-        .catch(() => {
-          Alert.alert('Erro', 'Não foi possível carregar os dados do Pokémon');
-        });
+  const getVideos = async() => {
+    const result = await axios.get('https://www.googleapis.com/youtube/v3/search?q=boating&key=AIzaSyD7XZQf-k9SuLkH2tLFHbJgt_y8nD6wLHI')
+    console.log(result)
   }
-
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <View style={styles.topo}>
-          <Text style={styles.topoTitulo}>ESCOLHA SEU POKÉMON</Text>
-        </View>
-
-        {pokemonEscolhido != null && (
-          <View style={styles.pokemonBox}>
-            <Text style={styles.pokemonNome}>Nome: {pokemonEscolhido.nome}</Text>
-            <Text style={styles.pokemonPeso}>Peso: {pokemonEscolhido.peso}</Text>
-
-            <Image resizeMode="stretch" source={{uri:pokemonEscolhido.img}} style={styles.pokemonImg} />
-          </View>
-        )}
-
-        {pokemonsIniciais.map( data => (
-          <View style={styles.cardContainer}>
-            <Text style={styles.cardTitle}>{data.q}</Text>
-            <Button title="Dados do pokémon" onPress={()=>getPokemonData(data.id)}/>
-          </View>
-        ))}
-      </ScrollView>
+      
+      <TextInput value={search} onChangeText={text => setSearch(text)} placeholder='Buscar Videos'/>
+      <TouchableOpacity onPress={getVideos}>
+        <Text>Buscar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff' },
-
-  topo: { height: 80, padding: 20, paddingTop: 40, marginBottom: 20, backgroundColor: '#e73e33' },
-  topoTitulo: { fontSize: 22, marginBottom: 10, color: '#fff', textAlign: 'center'},
-
-  cardContainer: { borderWidth: 1, borderColor: '#d5d5d5', borderRadius: 4, marginBottom: 10, marginHorizontal: 20, padding: 10 },
-  cardTitle: { fontSize: 22, marginBottom: 20, textAlign: 'center', color: '#656565' },
-
-  pokemonBox: { alignItems: 'center' },
-  pokemonNome: { fontSize: 22 },
-  pokemonPeso: { fontSize: 18 },
-  pokemonImg:{ width: 150, height: 150,}
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
--- api/api.js
+++ api/api.js
@@ -1,12 +1,30 @@
 const baseUrl = 'https://www.googleapis.com/youtube/v3/'
-
-export const getCommentsBook = async id => {
+export const getSearch = async id => {
 
     try {
-        const response = await get(`${baseUrl}/search=${id}`);
+        const response = await fetch(`${baseUrl}/search=${id}`);
         const responseData = await response.json();
         return responseData.data;
     } catch (err) {
         return console.log(err);
+    }
+};
+
+export const getVideos = async () => {
+    try {
+        //const token = await AsyncStorage.getItem("@token");
+        
+        const response = await fetch(`${baseUrl}/search?q=boating&key=AIzaSyD7XZQf-k9SuLkH2tLFHbJgt_y8nD6wLHI`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+
+        const responseData = await response.json();
+        return responseData.data;
+    } catch (error) {
+        console.log(error);
     }
 };
