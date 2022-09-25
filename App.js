696611a0add18c4024c09fc96d261157c19a10f6
correaa1 <bruno_correa_ds@hotmail.com>
2022-09-25 20:20:45 -0300

    melhorando as buscas

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react'
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import axios from 'axios'
-
+import api from './api/api'
 
 export default function App() {
   const [search, setSearch] = useState('');
 
   const getVideos = async() => {
-    const result = await axios.get('https://www.googleapis.com/youtube/v3/search?q=boating&key=AIzaSyD7XZQf-k9SuLkH2tLFHbJgt_y8nD6wLHI')
+    const result = await api.get(`https://www.googleapis.com/youtube/v3/search?${search}`, {
+      params:{
+       key: 'AIzaSyD7XZQf-k9SuLkH2tLFHbJgt_y8nD6wLHI',
+        maxResults: '7',
+        part: 'snippet',
+        order: 'title',
+        regionCode: 'BR',
+        q:'barco',
+        relevanceLanguage:'pt'
+      }
+    })
     console.log(result)
+    setSearch(result)
   }
+  
   return (
     <View style={styles.container}>
       
--- api/api.js
+++ api/api.js
@@ -1,30 +1,11 @@
-const baseUrl = 'https://www.googleapis.com/youtube/v3/'
-export const getSearch = async id => {
+import axios from 'axios'
 
-    try {
-        const response = await fetch(`${baseUrl}/search=${id}`);
-        const responseData = await response.json();
-        return responseData.data;
-    } catch (err) {
-        return console.log(err);
-    }
-};
+const api = axios.create({
+   baseUrl: 'https://www.googleapis.com/youtube/v3/'
 
-export const getVideos = async () => {
-    try {
-        //const token = await AsyncStorage.getItem("@token");
-        
-        const response = await fetch(`${baseUrl}/search?q=boating&key=AIzaSyD7XZQf-k9SuLkH2tLFHbJgt_y8nD6wLHI`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        });
+}) 
 
-        const responseData = await response.json();
-        return responseData.data;
-    } catch (error) {
-        console.log(error);
-    }
-};
+ 
+
+  
+export default api;
